83ab7bfaec7f15b87c1435c3fab584d8eb9b71ce
Maca <maca@brutalrocks.com>
2023-05-30 11:41:31 -0400

    Fix: routes/users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const path = require('path');
 const fs = require('fs');
-const users = require('../data/users.json');
+// const users = require('../data/users.json');
 
 const filePath = path.join(__dirname, '../data/users.json');
 
@@ -12,18 +12,30 @@ router.get('/', (req, res) => {
       res.send(userData);
     });
   } catch (error) {
-    res.status(500).send({ message: 'An error has ocurred on the server' });
+    res.status(500).send({ message: 'Ha ocurrido un error en el servidor' });
   }
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
 
-  if (!users[id]) {
-    res.status(404).send({ message: 'ID de usuario no encontrado' });
-    return;
-  }
-  res.status(200).send(users);
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Error al leer el archivo JSON:', err);
+      res.status(500).send({ message: 'Ha ocurrido un error en el servidor' });
+      return;
+    }
+
+    const users = JSON.parse(data);
+    const user = users.find((userValue) => userValue._id === id);
+
+    if (!user) {
+      res.status(404).send({ message: 'ID de usuario no encontrado' });
+      return;
+    }
+
+    res.status(200).send(user);
+  });
 });
 
 module.exports = router;
